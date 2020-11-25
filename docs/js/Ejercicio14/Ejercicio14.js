8f06f259066ed8bbbdc07c0e692ad9c5d63d64cc
UO270571 <uo270571@uniovi.es>
2020-11-25 13:38:27 +0100

    Ejercicio 14

---
+++ docs/js/Ejercicio14/Ejercicio14.js
@@ -0,0 +1,31 @@
+"use strict";
+function draw() {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+
+    ctx.canvas.width = window.innerWidth;
+
+    var offsetX = canvas.getBoundingClientRect().left;
+    var offsetY = canvas.getBoundingClientRect().top;
+
+    var drawing = false;
+
+    canvas.addEventListener("mousemove", function(e) {
+        if(drawing) {
+            ctx.lineTo(e.clientX - offsetX, e.clientY - offsetY);
+            ctx.stroke();
+        }
+    }, false);
+
+    canvas.addEventListener("mousedown", function(e) {
+        drawing = true;
+        ctx.beginPath();
+        ctx.moveTo(e.clientX - offsetX, e.clientY - offsetY);
+    }, false);
+
+    window.addEventListener("mouseup", function(e) {
+        drawing = false;
+    }, false);
+}
+
+
