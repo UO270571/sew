56feec5967c60086d1fe714c36b9ab63af8e058f
UO270571 <uo270571@uniovi.es>
2020-11-27 15:03:53 +0100

    Terminado ejercicio 14

--- docs/js/Ejercicio14/Ejercicio14.js
+++ docs/js/Ejercicio14/Ejercicio14.js
@@ -1,61 +1,152 @@
 "use strict";
-function draw() {
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
 
-    ctx.canvas.width = window.innerWidth;
+class Pizarra {
 
-    var offsetX = canvas.getBoundingClientRect().left;
-    var offsetY = canvas.getBoundingClientRect().top;
+    load() {
+        this.canvas = document.getElementById('canvas');
+        this.ctx = canvas.getContext('2d');
 
-    var drawing = false;
+        this.canvas.width = window.innerWidth;
+        var bottomHeight = $("#archivos").outerHeight(true);
+        var topHeight = $("#ajustes").outerHeight(true) + $("h1").outerHeight(true);
+        this.canvas.height = window.innerHeight - topHeight - bottomHeight - 2;
 
-    function getPos(e) {
+        this.offsetX = canvas.getBoundingClientRect().left;
+        this.offsetY = canvas.getBoundingClientRect().top;
+
+        this.drawing = false;
+
+        document.getElementById("grosor").value = 3;
+        this.cambiarPropiedad("lineWidth", "grosor");
+
+        this.cargarEstado();
+
+        this.canvas.onmousemove = this.draw.bind(this);
+        this.canvas.ontouchmove = this.draw.bind(this);
+        this.canvas.onmousedown = this.startDrawing.bind(this); 
+        this.canvas.ontouchstart = this.startDrawing.bind(this);
+        window.onmouseup = this.stopDrawing.bind(this);
+        window.ontouchend = this.stopDrawing.bind(this);
+        window.onresize = this.resize.bind(this);
+        window.onbeforeunload = this.guardarEstado.bind(this);
+    }
+
+    resize() {
+        var url = this.canvas.toDataURL();
+        this.canvas.width = window.innerWidth;
+        var bottomHeight = $("#archivos").outerHeight(true);
+        var topHeight = $("#ajustes").outerHeight(true) + $("h1").outerHeight(true);
+        this.canvas.height = window.innerHeight - topHeight - bottomHeight - 2;
+        var img = new Image();
+        img.onload = function() {
+            this.ctx.drawImage(img, 0, 0, this.canvas.width, this.canvas.height);
+        }.bind(this);
+        img.src = url;
+        this.cambiarPropiedad("lineWidth", "grosor");
+        this.cambiarPropiedad("strokeStyle", "color");
+        this.cambiarPropiedad("globalCompositeOperation", "herramienta");
+        this.offsetX = this.canvas.getBoundingClientRect().left;
+        this.offsetY = this.canvas.getBoundingClientRect().top;
+    }
+
+    cargarImagen(files) {
+        var reader = new FileReader();
+        reader.onload = function() {
+            var img = new Image();
+            img.onload = function(){
+                this.ctx.globalCompositeOperation = "source-over";
+                this.ctx.drawImage(img, 0, 0);
+                this.cambiarPropiedad("globalCompositeOperation", "herramienta");
+            }.bind(this);
+            img.src = reader.result;
+        }.bind(this);
+        reader.readAsDataURL(files[0]); 
+    }
+
+    cambiarPropiedad(attr, propiedad) {
+        this.ctx[attr] = document.getElementById(propiedad).value;
+    }
+
+    guardarEstado() {
+        localStorage.setItem('saved', document.getElementById("canvas").toDataURL());
+        localStorage.setItem('color', document.getElementById("color").value);
+        localStorage.setItem('grosor', document.getElementById("grosor").value);
+        localStorage.setItem('herramienta', document.getElementById("herramienta").value);
+    }
+
+    cargarEstado() {
+        var grosor = localStorage.getItem("grosor");
+        if(grosor) {
+            document.getElementById("grosor").value = grosor;
+            this.cambiarPropiedad("lineWidth", "grosor");
+        }
+
+        var color = localStorage.getItem("color");
+        if(color) {
+            document.getElementById("color").value = color;
+            this.cambiarPropiedad("strokeStyle", "color");
+        }
+
+        var herramienta = localStorage.getItem("herramienta");
+        if(herramienta) {
+            document.getElementById("herramienta").value = herramienta;
+            this.cambiarPropiedad("globalCompositeOperation", "herramienta");
+        }
+
+        var url = localStorage.getItem("saved");
+        if(url) {
+            var img = new Image();
+            img.onload = function(){
+                this.ctx.globalCompositeOperation = "source-over";
+                this.ctx.drawImage(img, 0, 0);
+                this.cambiarPropiedad("globalCompositeOperation", "herramienta");
+            }.bind(this);
+            img.src = url;
+        }
+    }
+
+    borrarTodo() {
+        this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+    getPos(e) {
         var pos = new Object();
-        if(e instanceof TouchEvent) {
-            pos.x = e.targetTouches[0].clientX - offsetX;
-            pos.y = e.targetTouches[0].clientY - offsetY;
+        if(e.targetTouches) {
+            pos.x = e.targetTouches[0].clientX - this.offsetX;
+            pos.y = e.targetTouches[0].clientY - this.offsetY;
         } else {
-            pos.x = e.clientX - offsetX;
-            pos.y = e.clientY - offsetY;
+            pos.x = e.clientX - this.offsetX;
+            pos.y = e.clientY - this.offsetY;
         }
         return pos;
     }
 
-    function draw(e) {
-        if(drawing) {
-            var pos = getPos(e);
-            ctx.lineTo(pos.x, pos.y);
-            ctx.stroke();
+    draw(e) {
+        if(this.drawing) {
+            var pos = this.getPos(e);
+            this.ctx.lineTo(pos.x, pos.y);
+            this.ctx.stroke();
         }
     }
 
-    function startDrawing(e) {
-        drawing = true;
-        var pos = getPos(e);
-        ctx.beginPath();
-        ctx.moveTo(pos.x, pos.y);
+    startDrawing(e) {
+        this.drawing = true;
+        var pos = this.getPos(e);
+        this.ctx.beginPath();
+        this.ctx.moveTo(pos.x, pos.y);
     }
 
-    function stopDrawing(e) {
-        drawing = false;
+    stopDrawing(e) {
+        this.drawing = false;
     }
 
-    window.addEventListener("deviceorientation", function(e) {
-        ctx.canvas.width = window.innerWidth;
-    }, true);
-
-    canvas.addEventListener("mousemove", draw, false);
-
-    canvas.addEventListener("mousedown", startDrawing, false);
-
-    window.addEventListener("mouseup", stopDrawing, false);
-
-    canvas.addEventListener("touchmove", draw, false);
-
-    canvas.addEventListener("touchstart", startDrawing, false);
-
-    window.addEventListener("touchend", stopDrawing, false);
+    download() {
+        var enlace = document.createElement("a");
+        enlace.href = this.canvas.toDataURL();
+        enlace.download = "pizarra.png";
+        enlace.click();
+    }
 }
 
+var pizarra = new Pizarra();
 
